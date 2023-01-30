d48846b89e34a74236a79d51bc253ff43d6ca43b
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-01-30 16:56:15 +0100

    created the CategorySeeder

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $categories = [
+            'Attualità',
+            'Mondo dello spettacolo',
+            'Politica',
+            'Informatica',
+            'Finanza',
+            'Gossip',
+            'Finanza',
+            'Cinema',
+            'Scuola',
+            'Imprenditoria',
+        ];
+
+        foreach ($categories as $category) {
+            Category::create([
+                'slug' => Category::getSLug($category),
+                'name' => $category,
+                'description' => $faker->paragraphs(rand(2, 4), true)
+            ]);
+        }
+    }
+}
