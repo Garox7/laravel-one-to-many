697ed816cc05dee2856d9761c46e66f6b5641517
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-01-30 16:58:08 +0100

    updated the PostSeeder

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Post;
+use App\Category;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,14 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i < 4; $i++) {
+        $category = Category::all('id')->all();
+
+        for ($i=0; $i < 20; $i++) {
             $title = $faker->words(rand(3, 7), true);
 
             $post = new Post();
-            $post->slug = $title;
+            $post->category_id = $faker->randomElement($category)->id;
+            $post->slug = Post::getSLug($title);
             $post->title = $title;
             $post->image = 'http://picsum.photos/id/' . rand(0,1000) . '/500/400';
             $post->content = $faker->paragraphs(rand(5,15), true);
